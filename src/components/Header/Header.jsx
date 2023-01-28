d52ad5309ff9f0d23dfbc04ec5fc8a75d38cd35b
CSarroca <sarroca93@gmail.com>
2023-01-28 15:40:38 +0100

    Created Header component and added to app

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import HeaderStyled from "./HeaderStyled";
+
+const Header = () => {
+  return (
+    <HeaderStyled>
+      <h1 className="header-title">Movies App</h1>
+      <img
+        className="header-logo"
+        src="/images/popcorn2.png"
+        alt="Logo of Movies app"
+        height={80}
+        width={80}
+      />
+    </HeaderStyled>
+  );
+};
+
+export default Header;
