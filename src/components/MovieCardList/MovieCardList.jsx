8f3e1417727203649f8cced46913a8db2ea40c84
CSarroca <sarroca93@gmail.com>
2023-01-29 23:10:55 +0100

    Created HomePage page

--- src/components/MovieCardList/MovieCardList.jsx
+++ src/components/MovieCardList/MovieCardList.jsx
@@ -1,14 +1,11 @@
-import { useSelector } from "react-redux";
 import MovieCard from "../MovieCard/MovieCard";
 import MovieCardListStyled from "./MovieCardListStyled";
 
-const MovieCardList = () => {
-  const { movies } = useSelector((state) => state.movies);
-
+const MovieCardList = ({ list }) => {
   return (
     <MovieCardListStyled>
       <ul className="cards-list">
-        {movies.map((movie) => (
+        {list.map((movie) => (
           <li key={movie.id} className="cards-list__card">
             <MovieCard movie={movie} />
           </li>
