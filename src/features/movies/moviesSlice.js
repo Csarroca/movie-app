68b909cce4f824a46e2ba281e40fc88668348b5d
CSarroca <sarroca93@gmail.com>
2023-01-29 01:01:39 +0100

    Created reducer for add a movie

--- src/features/movies/moviesSlice.js
+++ src/features/movies/moviesSlice.js
@@ -3,14 +3,21 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   movies: [],
   isLoading: false,
+  id: 0,
+  error: null,
 };
 
 export const moviesSlice = createSlice({
   name: "movies",
   initialState,
-  reducers: {},
+  reducers: {
+    addMovie: (state, { payload }) => {
+      state.movies.push({ ...payload, id: state.id });
+      state.id += 1;
+    },
+  },
 });
 
-export const {} = moviesSlice.actions;
+export const { addMovie } = moviesSlice.actions;
 
 export const moviesReducer = moviesSlice.reducer;
