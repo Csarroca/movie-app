0a8e966684c33e98f26ea4e7139436e688582e9a
CSarroca <sarroca93@gmail.com>
2023-01-29 23:38:38 +0100

    Created Loader component and added spinner gif

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+const Loader = ({ isLoading }) => {
+  return (
+    isLoading && (
+      <div className="loading-indicator">
+        <img
+          src="/images/spinner.gif"
+          alt="Loading..."
+          height={40}
+          width={40}
+        />
+      </div>
+    )
+  );
+};
+
+export default Loader;
