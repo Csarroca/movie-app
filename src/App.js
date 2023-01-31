1e736cf3705ada8e5ae1f33d65aca5e00ed02152
CSarroca <sarroca93@gmail.com>
2023-01-31 22:58:16 +0100

    Added filteredPage route to app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
+import FilteredPage from "./pages/FilteredPage/FilteredPage";
 
 function App() {
   return (
     <>
       <Header />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
+
+        <Route path="/genre/:genre" element={<FilteredPage />} />
       </Routes>
     </>
   );
