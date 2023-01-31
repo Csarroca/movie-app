1c74ba5187e1a4d4ede8f3b9aedf221cb72cfacb
CSarroca <sarroca93@gmail.com>
2023-01-31 22:46:17 +0100

    Refactored MovieCard for update movie with conditional rendering

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { changeWatchedMovie, deleteMovie } from "../../features/movies/useApi";
+import useApi from "../../features/movies/useApi";
 import Button from "../Button/Button";
+import Input from "../Input/Input";
 import MovieCardStyled from "./MovieCardStyled";
 
 const MovieCard = ({ movie }) => {
   const dispatch = useDispatch();
-
+  const [openUpdate, setOpenUpdate] = useState(false);
+  const { deleteMovie, changeWatchedMovie, updateMovie } = useApi();
   const { name, genres, watched, id } = movie;
+
+  const [inputValue, setInputValue] = useState(movie.name);
 
   const toggleWhatched = () => {
     dispatch(changeWatchedMovie(id));
@@ -18,44 +23,75 @@ const MovieCard = ({ movie }) => {
     dispatch(deleteMovie(id));
   };
 
+  const handleEdit = () => {
+    dispatch(updateMovie({ ...movie, name: inputValue }));
+    setOpenUpdate(false);
+  };
   return (
     <MovieCardStyled className="card" data-testid="card">
       <div className="card-info">
-        <h2 className="title">{name}</h2>
+        {openUpdate ? (
+          <div className="card-info__input-edit">
+            <Input
+              placeholder="Edit the movie's name"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              name="editName"
+            />
 
-        <ul className="card-info__genres">
-          {genres.map((genre) => {
-            return (
-              <li className="tag" key={genre}>
-                {genre}
-              </li>
-            );
-          })}
-        </ul>
+            <Button
+              className="save-button"
+              buttonText="edit"
+              type="button"
+              onClick={handleEdit}
+            />
+          </div>
+        ) : (
+          <>
+            <h2 className="title">{name}</h2>
 
-        <div>
-          <input
-            id={name}
-            data-testid="checkbox"
-            className="checkbox"
-            name={name}
-            type="checkbox"
-            onChange={toggleWhatched}
-            required="required"
-            checked={watched}
-          />
-          <span />
-          <label htmlFor={name} className="checkbox-label">
-            watched movie
-          </label>
-        </div>
+            <ul className="card-info__genres">
+              {genres.map((genre) => {
+                return (
+                  <li className="tag" key={genre}>
+                    {genre}
+                  </li>
+                );
+              })}
+            </ul>
 
-        <Button
-          className="delete-button"
-          buttonText="delete"
-          type="button"
-          onClick={handleDelete}
-        />
+            <div>
+              <input
+                id={name}
+                data-testid="checkbox"
+                className="checkbox"
+                name={name}
+                type="checkbox"
+                onChange={toggleWhatched}
+                required="required"
+                checked={watched}
+              />
+              <span />
+              <label htmlFor={name} className="checkbox-label">
+                watched movie
+              </label>
+            </div>
+
+            <Button
+              className="delete-button"
+              buttonText="delete"
+              type="button"
+              onClick={handleDelete}
+            />
+
+            <Button
+              className="save-button"
+              buttonText="edit"
+              type="button"
+              onClick={() => setOpenUpdate(true)}
+            />
+          </>
+        )}
       </div>
     </MovieCardStyled>
   );
