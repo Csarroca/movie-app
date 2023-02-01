0f4f020e9eb670651fc596ae2a163057155db53a
CSarroca <sarroca93@gmail.com>
2023-02-01 23:38:54 +0100

    Changed size of input component

--- src/components/Input/InputStyled.jsx
+++ src/components/Input/InputStyled.jsx
@@ -3,22 +3,22 @@ import styled from "styled-components";
 const InputStyled = styled.div`
   padding: 20px;
   font-weight: bold;
+  width: 290px;
 
   .input-container {
     gap: 10px;
     display: flex;
     flex-direction: column;
-    width: 213px;
     color: ${({ theme }) => theme.colors.brand.primary};
   }
 
   input {
     all: unset;
     cursor: pointer;
-    padding: 5px;
+    padding: 20px;
     border: 1px solid rgba(0, 0, 0, 0.16);
     border-radius: 3px;
-    font-size: 10px;
+    font-size: 14px;
     font-weight: lighter;
   }
 
