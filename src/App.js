372f57c86cf156e50846459b5eae1861fd8a9c95
CSarroca <sarroca93@gmail.com>
2023-02-01 23:37:45 +0100

    Created AppStlyed component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import FilteredPage from "./pages/FilteredPage/FilteredPage";
+import AppStyled from "./AppStyled";
 
 function App() {
   return (
-    <>
+    <AppStyled>
       <Header />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/" />} />
 
-        <Route path="/genre/:genre" element={<FilteredPage />} />
+        <Route path="/:genre" element={<FilteredPage />} />
       </Routes>
-    </>
+    </AppStyled>
   );
 }
 
