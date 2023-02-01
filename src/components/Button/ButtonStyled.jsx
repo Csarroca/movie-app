c436c3b1776d9d167930bdab065ec78b2eadef9a
CSarroca <sarroca93@gmail.com>
2023-02-02 00:50:35 +0100

    Added react-icons package

--- src/components/Button/ButtonStyled.jsx
+++ src/components/Button/ButtonStyled.jsx
@@ -2,15 +2,16 @@ import styled from "styled-components";
 
 const ButtonStyled = styled.button`
   align-self: center;
-  width: 80px;
   background-color: ${({ theme }) => theme.colors.brand.primary};
   color: white;
-  height: 25px;
   border-radius: 30px;
   cursor: pointer;
   border: none;
   outline: none;
-  font-size: 0.6rem;
+  font-size: 1rem;
+  line-height: 1rem;
+  height: 1.5rem;
+  padding: 0px 15px;
 
   :hover {
     background-color: ${({ theme }) => theme.colors.brand.secundary};
