c7616b77876ca5731e09682dbb4387ab63d1c9ea
CSarroca <sarroca93@gmail.com>
2023-02-02 22:29:59 +0100

    Added MovieCard style

--- src/components/MovieCard/MovieCardStyled.jsx
+++ src/components/MovieCard/MovieCardStyled.jsx
@@ -1,22 +1,75 @@
 import styled from "styled-components";
 
 const MovieCardStyled = styled.article`
-  padding-top: 2em;
+  background-color: ${({ theme }) => theme.colors.brand.tertiary};
+  border-radius: 5%;
+  max-width: 360px;
 
   .card-info {
+    gap: 8px;
+    padding: 20px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    box-shadow: ${({ theme }) => theme.boxShadow.regular};
-    width: 200px;
+
+    &__input-edit {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    &__title {
+      margin-bottom: 8px;
+      color: ${({ theme }) => theme.colors.brand.primary};
+      text-transform: uppercase;
+      text-align: center;
+    }
+
+    &__group {
+      display: flex;
+      gap: 20px;
+      flex-direction: row;
+      align-items: center;
+    }
+
+    &__tag {
+      text-transform: capitalize;
+      font-weight: bold;
+    }
+
+    &__genres {
+      display: flex;
+      flex-direction: column;
+      max-width: 100%;
+      gap: 20px;
+      color: ${({ theme }) => theme.colors.brand.primary};
+    }
+  }
+
+  .checkbox {
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.brand.secundary};
+
+    &-container {
+      display: flex;
+      gap: 5px;
+    }
   }
 
   .delete-button {
-    margin: 10px;
+    all: unset;
+    color: ${({ theme }) => theme.colors.brand.primary};
+    font-size: 30px;
   }
 
-  .title {
+  .save-button {
+    all: unset;
     color: ${({ theme }) => theme.colors.brand.primary};
+    font-size: 30px;
+  }
+
+  .card-info__button-container {
+    display: flex;
+    gap: 150px;
   }
 `;
 
