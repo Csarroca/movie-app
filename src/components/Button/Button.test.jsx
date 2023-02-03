1d324cb091ffafb0f82ca4d93f536e6fc3902de5
CSarroca <sarroca93@gmail.com>
2023-02-03 23:12:51 +0100

    Added test for button

--- src/components/Button/Button.test.jsx
+++ src/components/Button/Button.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Button from "./Button";
 import userEvent from "@testing-library/user-event";
-import { Wrapper } from "../../test-utils/render/render";
+import { Wrapper } from "../../test-utils/Wrapper/Wrapper";
 
 describe("Given a AnimatedButton component", () => {
   describe("When instantiated with onClick", () => {
